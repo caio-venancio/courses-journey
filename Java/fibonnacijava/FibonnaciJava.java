45863c5340b9fa46c9ce0183eff59583753c36b1
caio-venancio <caio.venancio784@gmail.com>
2025-11-10 22:58:17 -0300

    feat:adicionar algoritmo recursivo para sequência de fibonnaci

--- Java/fibonnacijava/FibonnaciJava.java
+++ Java/fibonnacijava/FibonnaciJava.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class FibonnaciJava {
     
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite algum posição de número da sequência de fibonnaci:  ");
+        int pos = scanner.nextInt();
+        scanner.close();
+
+        FibonnaciJava fb = new FibonnaciJava();
+        System.out.println(fb.fibbonacci(pos));
+    }
+
+    public int fibbonacci(int pos){
+        if(pos == 0){
+            return 0;
+        } else if (pos == 1){
+            return 1;
+        } else if (pos == 2){
+            return 1;
+        }
+
+        return fibbonacci(pos-1) + fibbonacci(pos-2);
+    }
 }
--- Java/validarparentesesjava/ValidarParenteses.java
+++ Java/validarparentesesjava/ValidarParenteses.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class ValidarParenteses {
 
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite texto para testar parênteses:  ");
+        String teste = scanner.nextLine();
+        scanner.close();
+
+
+    }
+
+    
 }
