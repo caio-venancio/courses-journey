b9463fd54f0a18ff1897c28b10b3dcdb5eff5fda
caio-venancio <caio.venancio784@gmail.com>
2025-11-11 22:19:14 -0300

    feat: adicionar rotação de array para direita

---
+++ Java/rotatearrayjava/RotateArray.java
@@ -0,0 +1,43 @@
+//algoritmo das 3 reversões
+// Usa bem o cache, usa menos branches, não precisa checar borda, e quando k cresce o tempo não cresce
+
+public class RotateArray {
+
+    public static void rotateRight(int[] arr, int k) {
+        int n = arr.length;
+        // Garante que k está dentro dos limites do array
+        k = k % n;
+
+        if (k == 0) {
+            return; // Nenhuma rotação necessária
+        }
+
+        // Inverte todo o array
+        reverse(arr, 0, n - 1);
+        // Inverte os primeiros k elementos
+        reverse(arr, 0, k - 1);
+        // Inverte os elementos restantes
+        reverse(arr, k, n - 1);
+    }
+
+    
+
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 2;
+        rotateRight(arr, k);
+        // Imprime o array rotacionado: {4, 5, 1, 2, 3}
+        System.out.println(java.util.Arrays.toString(arr));
+    }
+
+}
