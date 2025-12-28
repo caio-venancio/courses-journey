098aa59be50b9799853c4837975f9bb0b9d500c1
caio-venancio <caio.venancio784@gmail.com>
2025-12-28 14:23:41 -0300

    feat: adicionar exemplo de async

---
+++ javascript/basics/promises/async.js
@@ -0,0 +1,27 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+
+// async function name(param0, param1, /* …, */ paramN) {
+//   statements
+// }
+
+// An async function declaration creates an AsyncFunction object. Each time when an async function is called, it returns a new Promise which will be resolved with the value returned by the async function, or rejected with an exception uncaught within the async function.
+
+// Async functions can contain zero or more await expressions. Await expressions make promise-returning functions behave as though they're synchronous by suspending execution until the returned promise is fulfilled or rejected. The resolved value of the promise is treated as the return value of the await expression. Use of async and await enables the use of ordinary try / catch blocks around asynchronous code.
+
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("resolved");
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log("calling");
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+  // Expected output: "resolved"
+}
+
+
+asyncCall();
