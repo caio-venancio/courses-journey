103c8e744bd26a69cec73c766958a1cb73124e2f
caio-venancio <caio.venancio784@gmail.com>
2025-12-31 00:03:27 -0300

    feat: adicionar vectors folder

--- c++/bibliotecas/STL/sort.cpp
+++ c++/bibliotecas/STL/sort.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+// It is basically a header file that includes every standard library. In programming contests, using this file is a good idea, when you want to reduce the time wasted in doing chores; especially when your rank is time sensitive. 
 using namespace std;
 
 int main() {
