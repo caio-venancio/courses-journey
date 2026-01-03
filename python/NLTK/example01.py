cf5370565a0e66667a0d3a4375f2ad3222e09aec
caio-venancio <caio.venancio784@gmail.com>
2026-01-03 10:49:21 -0300

    feat: adicionar exemplo de nltk

---
+++ python/NLTK/example01.py
@@ -0,0 +1,12 @@
+import nltk
+
+# Download required NLTK data
+# nltk.download('punkt_tab')
+# nltk.download('averaged_perceptron_tagger_eng')
+
+sentence = """At eight o'clock on Thursday morning
+... Arthur didn't feel very good."""
+tokens = nltk.word_tokenize(sentence)
+print("tokens:", tokens)
+tagged = nltk.pos_tag(tokens)
+print("tagged", tagged[0:6])
