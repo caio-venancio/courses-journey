f77b8e9c3f43994027174b725b6d9919b0bef955
caio-venancio <caio.venancio784@gmail.com>
2026-01-05 22:56:29 -0300

    feat: adicionar comparação de arquivos iguais

---
+++ cmd/samefile.bat
@@ -0,0 +1,6 @@
+fc /b cmd\allfiles.bat cmd\allfiles.bat
+
+@REM fc: O comando para comparar arquivos.
+@REM /b: Indica uma comparação binária (byte a byte), ideal para qualquer tipo de arquivo.
+@REM arquivo1.ext: O caminho completo ou relativo do primeiro arquivo.
+@REM arquivo2.ext: O caminho completo ou relativo do segundo arquivo. 
