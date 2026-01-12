f580f3b5b2e647e0b873f3eb7c54cbe250e7052a
caio-venancio <caio.venancio784@gmail.com>
2026-01-12 19:21:06 -0300

    feat: adicionar newerDate.js

---
+++ javascript/basics/strings/newerDate.js
@@ -0,0 +1,16 @@
+let arrayDates = ["s18102025", "12122027y", "16032026"]
+
+function parseDate(str) {
+  const digits = str.replace(/\D/g, ""); // remove letras
+  const day = Number(digits.slice(0, 2));
+  const month = Number(digits.slice(2, 4)) - 1; // JS começa em 0
+  const year = Number(digits.slice(4, 8));
+
+  return new Date(year, month, day);
+}
+
+const oldestString = arrayDates.reduce((oldest, current) => {
+  return parseDate(current) < parseDate(oldest) ? current : oldest;
+});
+
+console.log(oldestString);
