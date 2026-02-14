12fd15e51584e2add40d5b38d903f216d3e183d6
caio-venancio <caio.venancio784@gmail.com>
2026-02-14 14:10:36 -0300

    feat: adicionar exemplo de nested layout

---
+++ typescript/frameworks/nextjs/my-app/src/app/features/addingLayout/page.tsx
@@ -0,0 +1,10 @@
+export default function addingLayout(){
+    return (<>
+        <div>
+            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">Hello, everyone!</h1>
+            <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+                Here's a custom layout.
+            </p>
+        </div>
+    </>)
+}
