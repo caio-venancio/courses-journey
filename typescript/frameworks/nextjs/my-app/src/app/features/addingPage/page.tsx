11fd3bf8375b40eb45e552f77f948e7f0f868d50
caio-venancio <caio.venancio784@gmail.com>
2026-02-13 22:41:36 -0300

    feat: adicionar página em next.js

---
+++ typescript/frameworks/nextjs/my-app/src/app/features/addingPage/page.tsx
@@ -0,0 +1,9 @@
+export default function addingPage(){
+    return (
+    <>
+        <h1>Como adicionar uma página em Next.js?</h1>
+        <h3>https://nextjs.org/docs/app/getting-started/layouts-and-pages</h3>
+        
+        <p>Coloque uma arquivo de página que retorne default export jsx dentro e o next.js reconhecerá</p>
+    </>)
+}
---
+++ typescript/frameworks/nextjs/readme.md
@@ -0,0 +1,8 @@
+https://nextjs.org/docs/app/getting-started/installation
+
+# To get started
+```
+npx create-next-app@latest my-app --yes
+cd my-app
+npm run dev
+```
