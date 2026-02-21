d83c4e7737b763ab8d9b792d550f15ce7d71c166
caio-venancio <caio.venancio784@gmail.com>
2026-02-20 22:57:04 -0300

    feat: adicionar modelagem de documentos

--- typescript/questionsAnalyser/src/core/models/document.ts
+++ typescript/questionsAnalyser/src/core/models/document.ts
@@ -17,6 +17,40 @@ export interface Question {
   answer?: string;
   livro?: string;
   capitulo?: string;
-  conteudo?: string;
-  area?: string;
+  area?: string[];
+  conteudo?: string[];
+  topic?: string[];
+  rank?: number[];
+  hasDocument?: boolean;
 }
+
+export interface Book {
+  id: string;
+  title: string;
+  chapters?: string[];
+  area?: string[];
+  conteudo?: string[];
+  topic?: string[];
+  hasDocument?: boolean;
+}
+
+export interface Chapter {
+  id: string;
+  title: string;
+  activities?: string[];
+  area?: string[];
+  conteudo?: string[];
+  topic?: string[];
+  hasDocument?: boolean;
+}
+
+export interface CommonAsked {
+  id: string;
+  title: string;
+  questions?: string[];
+  area?: string[];
+  conteudo?: string[];
+  topic?: string[];
+  hasDocument?: boolean;
+}
+
