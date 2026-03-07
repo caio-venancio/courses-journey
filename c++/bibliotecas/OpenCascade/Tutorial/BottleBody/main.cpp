99f667962fc992a750c8d09f0c94e7eba3776c60
caio-venancio <caio.venancio784@gmail.com>
2026-03-07 14:12:49 -0300

    feat: adicionar BottleBody opencascade tutorial

---
+++ c++/bibliotecas/OpenCascade/Tutorial/BottleBody/main.cpp
@@ -0,0 +1,119 @@
+#include <Standard_Type.hxx>
+#include <Standard_Transient.hxx>
+#include <GC_MakeSegment.hxx>
+#include <GC_MakeArcOfCircle.hxx>
+#include <gp_Pnt.hxx>
+#include <gp_Ax1.hxx>
+#include <TopoDS_Shape.hxx>
+#include <TopoDS_Edge.hxx>
+#include <TopoDS_Wire.hxx>
+#include <TopoDS_Face.hxx>
+#include <TopoDS.hxx>
+#include <BRepBuilderAPI_MakeFace.hxx>
+#include <BRepBuilderAPI_MakeEdge.hxx>
+#include <BRepBuilderAPI_MakeWire.hxx>
+#include <BRepBuilderAPI_Transform.hxx>
+#include <BRepPrimAPI_MakePrism.hxx>
+#include <BRepPrimAPI_MakeCylinder.hxx>
+#include <BRepFilletAPI_MakeFillet.hxx>
+#include <TopExp_Explorer.hxx>
+#include <BRepAlgoAPI_Fuse.hxx>
+#include <BRep_Tool.hxx>
+#include <Geom_Plane.hxx>
+// #include <BRepBuilderAPI.hxx>
+#include <BRepOffsetAPI_MakeThickSolid.hxx>
+
+TopoDS_Shape MakeBottle(const Standard_Real myWidth, const Standard_Real myHeight,
+                        const Standard_Real myThickness)
+{
+    //Defining Support Points
+    // To create the bottle's profile, you first create characteristic points with their coordinates as shown below in the (XOY) plane
+    gp_Pnt aPnt1(-myWidth / 2., 0, 0);
+    gp_Pnt aPnt2(-myWidth / 2., -myThickness / 4., 0);
+    gp_Pnt aPnt3(0, -myThickness / 2., 0);
+    gp_Pnt aPnt4(myWidth / 2., -myThickness / 4., 0);
+    gp_Pnt aPnt5(myWidth / 2., 0, 0);
+
+    //Profile: Defining the Geometry
+    Handle(Geom_TrimmedCurve) aArcOfCircle = GC_MakeArcOfCircle(aPnt2,aPnt3,aPnt4);
+    Handle(Geom_TrimmedCurve) aSegment1    = GC_MakeSegment(aPnt1, aPnt2);
+    Handle(Geom_TrimmedCurve) aSegment2    = GC_MakeSegment(aPnt4, aPnt5);
+
+    //Profile: Defining the Topology
+    TopoDS_Edge anEdge1 = BRepBuilderAPI_MakeEdge(aSegment1);
+    TopoDS_Edge anEdge2 = BRepBuilderAPI_MakeEdge(aArcOfCircle);
+    TopoDS_Edge anEdge3 = BRepBuilderAPI_MakeEdge(aSegment2);
+    TopoDS_Wire aWire = BRepBuilderAPI_MakeWire(anEdge1, anEdge2, anEdge3);
+
+    //Completing the Profile
+    gp_Ax1 xAxis = gp::OX();
+    gp_Trsf aTrsf;
+
+    aTrsf.SetMirror(xAxis);
+    BRepBuilderAPI_Transform aBRepTrsf(aWire, aTrsf);
+    TopoDS_Shape aMirroredShape = aBRepTrsf.Shape();
+    TopoDS_Wire aMirroredWire = TopoDS::Wire(aMirroredShape);
+
+    BRepBuilderAPI_MakeWire mkWire;
+    mkWire.Add(aWire);
+    mkWire.Add(aMirroredWire);
+    TopoDS_Wire myWireProfile = mkWire.Wire();
+
+    //body: 
+    //Prism the profile
+    TopoDS_Face myFaceProfile = BRepBuilderAPI_MakeFace(myWireProfile);
+    gp_Vec aPrismVec(0, 0, myHeight);
+    TopoDS_Shape myBody = BRepPrimAPI_MakePrism(myFaceProfile, aPrismVec);
+
+    //body: apply fillets
+    BRepFilletAPI_MakeFillet mkFillet(myBody);
+    TopExp_Explorer anEdgeExplorer(myBody, TopAbs_EDGE);
+    while(anEdgeExplorer.More()){
+        TopoDS_Edge anEdge = TopoDS::Edge(anEdgeExplorer.Current());
+        //Add edge to fillet algorithm
+        mkFillet.Add(myThickness / 12., anEdge);
+        anEdgeExplorer.Next();
+    }
+    myBody = mkFillet.Shape();
+
+    //body: adding neck
+    gp_Pnt neckLocation(0, 0, myHeight);
+    gp_Dir neckAxis = gp::DZ();
+    gp_Ax2 neckAx2(neckLocation, neckAxis);
+    Standard_Real myNeckRadius = myThickness / 4.;
+    Standard_Real myNeckHeight = myHeight / 10;
+    BRepPrimAPI_MakeCylinder MKCylinder(neckAx2, myNeckRadius, myNeckHeight);
+    TopoDS_Shape myNeck = MKCylinder.Shape();
+    myBody = BRepAlgoAPI_Fuse(myBody, myNeck);
+
+    //body: Creating a Hollowed Solid
+    TopoDS_Face faceToRemove;
+    Standard_Real zMax = -1;
+    for(TopExp_Explorer aFaceExplorer(myBody, TopAbs_FACE) ; aFaceExplorer.More() ; aFaceExplorer.Next()){
+        TopoDS_Face aFace = TopoDS::Face(aFaceExplorer.Current());
+        Handle(Geom_Surface) aSurface = BRep_Tool::Surface(aFace);
+        if(aSurface->DynamicType() == STANDARD_TYPE(Geom_Plane) ){
+            Handle(Geom_Plane) aPlane = Handle(Geom_Plane)::DownCast(aSurface);
+            gp_Pnt aPnt = aPlane->Location();
+            Standard_Real aZ = aPnt.Z();
+            if(aZ > zMax){
+                zMax = aZ;
+                faceToRemove = aFace;
+            }
+        }
+    }
+
+    TopTools_ListOfShape facesToRemove;
+    facesToRemove.Append(faceToRemove);
+    BRepOffsetAPI_MakeThickSolid aSolidMaker;
+    aSolidMaker.MakeThickSolidByJoin(myBody, facesToRemove, -myThickness / 50, 1.e-3);
+    myBody = aSolidMaker.Shape();
+
+    //threading: creating surfaces
+
+    return TopoDS_Shape();
+}
+
+int main(){
+    return 0;
+}
