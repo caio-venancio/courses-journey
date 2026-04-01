61eabfdbbb07bc49ec0fdab25d027a1882b5a8d1
caio-venancio <caio.venancio784@gmail.com>
2026-03-31 23:05:19 -0300

    feat: adicionar dynamic routes em site estático

--- typescript/frameworks/nextjs/my-app/src/app/features/[id]/page.tsx
+++ typescript/frameworks/nextjs/my-app/src/app/features/[id]/page.tsx
@@ -1,3 +1,13 @@
+export const dynamicParams = false
+
+export async function generateStaticParams() {
+  return [
+    { id: '1' },
+    { id: '2' },
+  ]
+  //adicionar fallback
+}
+
 export default async function Produto({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
 
